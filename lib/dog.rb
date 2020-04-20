fd99b9319a1920dda5d1fee3eb184cbb86834731
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-20 21:07:21 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+  def name
+    @this_dogs_name
+  end
+
+  def breed=(dog_breed)
+    @this_dog_breed = dog_breed
+  end
+  def breed
+    @this_dog_breed
+  end
+end
